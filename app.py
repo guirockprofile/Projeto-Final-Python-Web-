1c14f5f6388a14798c3470eb1a94b9723ea3c5d9
guirockprofile <guirockprofile@gmail.com>
2022-10-31 21:26:57 +0000

    flask instalado

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
 def home():
     return 'Hello web'
-app.run()
+
+
+@app.route('/bye')
+def bye():
+    returm 'Bye'
+app.run(debug=True)
