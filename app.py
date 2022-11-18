802946167f3112c08821110125eade73f28cac26
guirockprofile <guirockprofile@gmail.com>
2022-11-18 20:50:43 +0000

    Modificações feitas na sala de aula

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+tasks = [
+    {'name': 'Estudar', 'finished': False},
+    {'name': 'Dormir', 'finished': True}
+    {'name': 'Comer', 'finished': True}
+]
+
 @app.route('/')
 def home():
     #templates
-    return render_template('home.html')
+    return render_template('home.html', tasks=tasks)
 
 @app.route('/bye')
 def bye():
